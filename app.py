b4b416bdcc30d8753bb572d3b091d0cfcbbdec71
Renzo Lucioni <renzolucioni@gmail.com>
2025-06-01 15:48:00 -0400

    expand to nhk

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import logging
 import os
+import random
 import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
 from logging.config import dictConfig
 from pathlib import Path
@@ -17,7 +19,6 @@ load_dotenv('.env.private')
 
 SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 SLACK_CHANNEL_ID = os.environ['SLACK_CHANNEL_ID']
-USE_CACHE = False
 
 dictConfig({
     'version': 1,
@@ -46,7 +47,13 @@ dictConfig({
 
 logger = logging.getLogger(__name__)
 
-slack = WebClient(token=SLACK_BOT_TOKEN)
+USE_CACHE = True
+HEADERS = {
+    'User-Agent': (
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/136.0.0.0 Safari/537.36'
+    )
+}
 
 # https://ai.google.dev/gemini-api/docs/pricing
 MODELS = {
@@ -71,6 +78,8 @@ gemini = genai.Client(
     http_options=genai.types.HttpOptions(timeout=120 * 1000)
 )
 
+slack = WebClient(token=SLACK_BOT_TOKEN)
+
 
 class Timer:
     def __init__(self):
@@ -93,6 +102,131 @@ class ProgressMeter:
         if self.done % self.mod == 0:
             percent = round((self.done / self.total) * 100)
             logger.info(self.msg.format(done=self.done, total=self.total, percent=percent))
+
+
+def get_ap_items():
+    session = requests.Session()
+    session.headers.update(HEADERS)
+
+    home_response = session.get('https://apnews.com/')
+    home_soup = BeautifulSoup(home_response.text, 'lxml')
+    links = home_soup.select('.PagePromo-title')
+
+    logger.info(f'found {len(links)} links')
+
+    item_timer = Timer()
+    link_progress = ProgressMeter(len(links), msg='tried {done}/{total} links ({percent}%)')
+
+    items = {}
+    for link in links:
+        try:
+            a_tag = link.select_one('a')
+            if not a_tag:
+                logger.info('skipping link with no <a> tag')
+                continue
+
+            link_href = a_tag.get('href')
+            if '/article/' not in link_href:
+                logger.info(f'skipping non-article link: {link_href}')
+                continue
+
+            # Articles can be linked to multiple times. For example, a link can appear
+            # in a regular section on the homepage and also in a "popular" section.
+            # Prefer the link with the most information in the title.
+            article_title = link.get_text(strip=True)
+            known_item = items.get(link_href)
+            if known_item and len(known_item['title']) >= len(article_title):
+                logger.info(f'skipping known article link: {link_href}')
+                continue
+
+            logger.info(f'getting content for article link: {link_href} ({article_title})')
+
+            article_response = session.get(link_href)
+            article_soup = BeautifulSoup(article_response.text, 'html.parser')
+            p_tags = article_soup.select('.RichTextStoryBody > p')
+
+            contents = []
+            for p_tag in p_tags:
+                # Avoid stripping internal whitespace (e.g., around <a> text)
+                content = p_tag.get_text().strip()
+
+                # Some articles end with notes below a horizontal rule of varying length. We don't need them.
+                if content.startswith('__') or content.startswith('——'):
+                    break
+
+                contents.append(content)
+
+            article_content = ' '.join(contents)
+
+            if not article_content:
+                logger.info(f'got empty content for article link: {link_href}')
+
+            items[link_href] = {
+                'title': article_title,
+                'url': link_href,
+                'content': article_content,
+            }
+        except:
+            logger.exception('failed to handle link')
+        finally:
+            link_progress.increment()
+            time.sleep(2)
+
+    item_timer.done()
+    logger.info(f'loaded {len(items)} items in {round(item_timer.latency, 2)}s')
+
+    return items
+
+
+def get_nhk_items():
+    session = requests.Session()
+    session.headers.update(HEADERS)
+
+    origin = 'https://www3.nhk.or.jp'
+    all_response = session.get(f'{origin}/nhkworld/data/en/news/all.json')
+    all_data = all_response.json()['data']
+
+    fresh_articles = []
+    now_ms = round(time.time() * 1000)
+    for article in all_data:
+        if now_ms - int(article['updated_at']) > (24 * 60 * 60 * 1000):
+            article_path = article['page_url']
+            logger.info(f'skipping stale article: {origin}{article_path}')
+            continue
+
+        fresh_articles.append(article)
+
+    logger.info(f'found {len(fresh_articles)} fresh articles')
+
+    items = {}
+    item_timer = Timer()
+
+    for article in fresh_articles:
+        try:
+            article_id = article['id']
+            logger.info(f'getting content for article {article_id}')
+
+            detail_response = session.get(f'{origin}/nhkworld/data/en/news/{article_id}.json')
+            detail_data = detail_response.json()['data']
+
+            article_path = detail_data['page_url']
+            item_url = f'{origin}{article_path}'
+            detail_soup = BeautifulSoup(detail_data['detail'], 'lxml')
+
+            items[item_url] = {
+                'title': detail_data['title'],
+                'url': item_url,
+                'content': detail_soup.get_text(),
+            }
+        except:
+            logger.exception('failed to handle article')
+        finally:
+            time.sleep(1)
+
+    item_timer.done()
+    logger.info(f'loaded {len(items)} items in {round(item_timer.latency, 2)}s')
+
+    return items
 
 
 def get_date():
@@ -178,14 +312,6 @@ def make_blocks(content):
 
 
 def run():
-    session = requests.Session()
-    session.headers.update({
-        'User-Agent': (
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
-            'Chrome/136.0.0.0 Safari/537.36'
-        )
-    })
-
     items_xml = None
     cache_path = Path('items.xml')
 
@@ -194,80 +320,45 @@ def run():
         items_xml = cache_path.read_text()
 
     if not items_xml:
-        home_response = session.get('https://apnews.com/')
-        home_soup = BeautifulSoup(home_response.text, 'lxml')
-        links = home_soup.select('.PagePromo-title')
+        sources = {
+            'AP': {
+                'crawler': get_ap_items,
+            },
+            'NHK': {
+                'crawler': get_nhk_items,
+            },
+        }
 
-        logger.info(f'found {len(links)} links')
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            futures = {}
+            for source, details in sources.items():
+                future = executor.submit(details['crawler'])
+                futures[future] = source
 
-        article_timer = Timer()
-        link_progress = ProgressMeter(len(links), msg='tried {done}/{total} links ({percent}%)')
+            for future in as_completed(futures):
+                source = futures[future]
 
-        articles = {}
-        for link in links:
-            try:
-                a_tag = link.select_one('a')
-                if not a_tag:
-                    logger.info('skipping link with no <a> tag')
+                try:
+                    sources[source]['items'] = future.result()
+                except:
+                    logger.exception(f'failed to get items for {source}')
                     continue
 
-                link_href = a_tag.get('href')
-                if '/article/' not in link_href:
-                    logger.info(f'skipping non-article link: {link_href}')
-                    continue
+        all_items = []
+        for details in sources.values():
+            source_items = details.get('items')
+            if source_items:
+                all_items.extend(list(source_items.values()))
 
-                # Articles can be linked to multiple times. For example, a link can appear
-                # in a regular section on the homepage and also in a "popular" section.
-                # Prefer the link with the most information in the title.
-                article_title = link.get_text(strip=True)
-                known_article = articles.get(link_href)
-                if known_article and len(known_article['title']) >= len(article_title):
-                    logger.info(f'skipping known article link: {link_href}')
-                    continue
+        random.shuffle(all_items)
 
-                logger.info(f'getting content for article link: {link_href} ({article_title})')
-
-                article_response = session.get(link_href)
-                article_soup = BeautifulSoup(article_response.text, 'html.parser')
-                p_tags = article_soup.select('.RichTextStoryBody > p')
-
-                contents = []
-                for p_tag in p_tags:
-                    # Avoid stripping internal whitespace (e.g., around <a> text)
-                    content = p_tag.get_text().strip()
-
-                    # Some articles end with notes below a horizontal rule of varying length. We don't need them.
-                    if content.startswith('__') or content.startswith('——'):
-                        break
-
-                    contents.append(content)
-
-                article_content = ' '.join(contents)
-
-                if not article_content:
-                    logger.info(f'got empty content for article link: {link_href}')
-
-                articles[link_href] = {
-                    'title': article_title,
-                    'url': link_href,
-                    'content': article_content,
-                }
-            except:
-                logger.exception('failed to handle link')
-            finally:
-                link_progress.increment()
-                time.sleep(2)
-
-        article_timer.done()
-        logger.info(f'loaded {len(articles)} articles in {round(article_timer.latency, 2)}s')
-
-        # TODO: map from full article url to short representation, then replace, to reduce mistakes
         items_xml = ''
-        for article in articles.values():
+        for item in all_items:
             item_xml = ITEM_XML_TEMPLATE.format(
-                title=article['title'],
-                url=article['url'],
-                content=article['content']
+                title=item['title'],
+                # TODO: map from full article url to short representation, then replace, to reduce mistakes
+                url=item['url'],
+                content=item['content']
             )
 
             items_xml += item_xml
